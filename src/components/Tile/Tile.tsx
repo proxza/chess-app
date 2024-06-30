eb6d4f0ea6d061b8d705358d39e33f03f5bf7ddf
proxza <proxza@gmail.com>
2024-06-30 10:36:02 +1000

    Add images

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,13 +1,22 @@
 import "./Tile.css";
 
 interface Props {
+  image?: string;
   number: number;
 }
 
-export default function Tile({ number }: Props) {
+export default function Tile({ image, number }: Props) {
   if (number % 2 === 0) {
-    return <div className="tile black-tile"></div>;
+    return (
+      <div className="tile black-tile">
+        <img src={image} alt="piece" />
+      </div>
+    );
   } else {
-    return <div className="tile white-tile"></div>;
+    return (
+      <div className="tile white-tile">
+        <img src={image} alt="piece" />
+      </div>
+    );
   }
 }
